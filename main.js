e17e389cbb6e287ec2c912882947739ddc94c13c
KatePelevina <64221065+KatePelevina@users.noreply.github.com>
2022-09-05 11:24:25 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+$('.single-item').slick({
+    nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
+});
+   
